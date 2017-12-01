92fc86c901045ce6c4ac671e970957834a4c6f00
Brenden Seidel <brenseid@gmail.com>
2017-12-01 13:17:52 -0500

    Handle status code errors

--- getUserProfile.js
+++ getUserProfile.js
@@ -3,7 +3,8 @@
 
 //Require https module
 const https = require('https');
-
+//Require http module for status codes
+const http = require('http');
 // Print Error Messages
 function printError(error) {
   console.error(error.message);
@@ -20,25 +21,30 @@ function getProfile(username) {
   // Connect to the API URL (https://teamtreehouse.com/username.json)
   try {
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-                                let body = "";
-                                //console.log(response.statusCode);
-                                
-                                // Read the data
-                                response.on('data', data => {
-                                  body += data.toString();
-                                });
-    
-                                response.on('end', () => {
-                                  try {
-                                    // Parse the data
-                                    const profile = JSON.parse(body);
-                                    //console.dir(profile);
-                                    // Print the data
-                                    printMessage(profile.profile_name, profile.badges.length, profile.points.JavaScript);                                    
-                                  } catch (error) {
-                                    printError(error);
-                                  }
-                                });
+                                if (response.statusCode === 200) {
+                                  let body = "";
+                                  //console.log(response.statusCode);
+                                  
+                                  // Read the data
+                                  response.on('data', data => {
+                                    body += data.toString();
+                                  });
+                                  response.on('end', () => {
+                                    try {
+                                      // Parse the data
+                                      const profile = JSON.parse(body);
+                                      //console.dir(profile);
+                                      // Print the data
+                                      printMessage(profile.profile_name, profile.badges.length, profile.points.JavaScript);                                    
+                                    } catch (error) {
+                                      printError(error);
+                                    }
+                                  });
+                                } else {
+                                  const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`
+                                  const statusCodeError = new Error(message);
+                                  printError(statusCodeError);
+                                }    
                               });    
   request.on('error', error => console.error(`Problem with request: ${error.message}`));
   } catch (error) {
